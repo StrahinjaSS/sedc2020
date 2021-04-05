55be1fee4abf801a29e9fdccfeffd2b0c68d21f6
Milenamaka <milena85.mm@yahoo.com>
2021-04-05 20:12:44 +0200

    teachers

--- js/projectTeachers.js
+++ js/projectTeachers.js
@@ -1,64 +1,70 @@
 const baseUrl1 = 'https://60642520f091970017785074.mockapi.io';
 function getTeachers(){
-    let req = new Request( baseUrl1 + '/teachers', ({method: 'GET'}));
-    fetch (req)
-    .then(res => {
-        console.log('res', res);
-        if (res.ok){
-            return res.json();
-        } else {
-            throw 'Greska';
-        }
-    })
-    .then(arr => {
-        console.log(arr);
-        renderTeachers();
-    })
+  let req = new Request( baseUrl1 + '/teachers', ({method: 'GET'}));
+  fetch (req)
+  .then(res => {
+      console.log('res', res);
+      if (res.ok){
+          return res.json();
+      } else {
+          throw 'Greska';
+      }
+  })
+  .then(arr => {
+      console.log(arr);
+      renderTeachers(arr);
+  });
 }
-function renderTeachers (arr){
-    $('.theachers').append(
-    `<div class="container">
-  <h2>Card Image</h2>
-  <p>Image at the top (card-img-top):</p>
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-    <div class="card-body">
-      <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
-    </div>
-  </div>
-  <br>
-  <p>Image at the top (card-img-top):</p>
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-    <div class="card-body">
-      <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
-    </div>
-  </div>
-  <br>
-  <p>Image at the top (card-img-top):</p>
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-    <div class="card-body">
-      <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
-    </div>
-  </div>
-  <br>
-  <p>Image at the top (card-img-top):</p>
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-    <div class="card-body">
-      <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
-    </div>
-  </div>
+getTeachers()
+function renderTeachers(arr) { 
+  arr.forEach(element => {      
+  let teacherItem = document.createElement ('div');
+  $(teacherItem).addClass('teachers-info');
+  $('.teachers-section').append ($(teacherItem));
+  
+  let teacherRewiev = document.createElement ('div');
+  $(teacherRewiev).addClass('teachers-rewiev');
+  $(teacherItem).append($(teacherRewiev));
+  
+  let teacherImg = document.createElement ('div');
+  $(teacherImg).addClass('teachers-img');
+  $(teacherRewiev).append($(teacherImg));
+  
+  let teacherImgImg = document.createElement ('img');
+  teacherImgImg.src = element.image;
+  $(teacherImg).append($(teacherImgImg));
+  
+  let teacherName = document.createElement ('div');
+  $(teacherName).addClass('teachers-name');
+  $(teacherRewiev).append($(teacherName));
+  
+  let teacherNameTitle = document.createElement ('h3');
+  teacherNameTitle.textContent = element.name;  
+  $(teacherName).append($(teacherNameTitle));
+  
+ let teacherNameQuote = document.createElement ('h4');
+  $(teacherNameQuote).text(element.quote);
+  $(teacherName).append($(teacherNameQuote));
+  
+  
+  let teacherIcon = document.createElement ('div');
+  $(teacherIcon).addClass('teachers-icon');
+  $(teacherRewiev).append($(teacherIcon));
+  
+  $(teacherIcon).append (
+          `<div class="icon"> <i class="fab fa-twitter"></i></div>
+          <div class="icon"><i class="fab fa-google"></i></div>
+          <div class="icon"> <i class="fab fa-behance"></i></div>`)
+    /*let teacherSocial = document.createElement('div');
+    let teacherSocialImg = document.createElement('img');
+    teacherSocialImg.src = el;
+    teacherSocialItem.appendChild(teacherSocial);
+    teacherSocial.appendChild(teacherSocialImg);*/
 
-</div>
+    /*behance: "https://www.behance.net"
+    google: "https://google.com"
+    twitter: "https://twitter.com"*/
 
+
+});
 }
