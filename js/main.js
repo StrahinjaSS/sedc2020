4127b7c154c13231eae05a7e9a8846aa43b1fc33
Excession27 <feboobef27@gmail.com>
2021-04-05 17:49:00 +0200

    Added courses

--- js/main.js
+++ js/main.js
@@ -9,13 +9,12 @@ const courses = "https://60642520f091970017785074.mockapi.io/courses"
 
 async function getData() {
     let teacherData = await getEndpoint(teachers);
-    let sliderData = getEndpoint(slider);
-    let reviewsData = getEndpoint(reviews);
-    let coursesData = getEndpoint(courses);
+    let sliderData = await getEndpoint(slider);
+    let reviewsData = await getEndpoint(reviews);
+    let coursesData = await getEndpoint(courses);
 
     teacherData.forEach(function (teacher) {
-        $('.teacher-cards-container').append(
-            `
+        $('.teacher-cards-container').append(`
         <div class="teacher-card">
         <a class="teacher-image-link" href="pages/teacher-page.html"><img src="${teacher.image}" alt="Teacher image"></a>
     
@@ -36,6 +35,85 @@ async function getData() {
         `
         );
 
+    });
+
+    // sliderData.forEach((slider) => {
+
+    // });
+
+    reviewsData.forEach((review) => {
+        $('.student-reviews-card-container').append(`
+        <div class="student-reviews-card">
+
+        <div class="student-card">
+            <div class="student-info">
+                <img src="images/tst-image1.jpg" alt=""     class="student-image">
+                <div class="student-info-text">
+                    <p class="student-name">Jackson</p>
+                    <p class="student-title">Shopify Developer</p>
+                </div>
+            </div>
+
+            <p class="student-testimonial">You really do help   young creative minds to get quality education and     professional job seach assistance. I'd recommend  it    to everyone!</p>
+            <div class="rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+
+            </div>
+        </div>
+        </div>
+        `)
+    });
+
+    coursesData.forEach((course) => {
+
+        let price = `${course.price} ${course.currency}`;
+        let priceClass = "nonfree";
+        if (course.price === 0) {
+            price = "Free";
+            priceClass = "free";
+        }
+
+        $('.popular-courses-card-container').append(`
+        <div    class="popular-courses-card">
+
+        <div class="course-image-and-info ${''}">
+
+
+            <div class="course-opacity-ribbon">
+                <span class="course-date">
+                    <i class="far fa-calendar-alt"></i>
+                    <span>${course.date}</span>
+                </span>
+                <span class="course-length">
+                    <i class="far fa-clock"></i>
+                    <span>${course.duration} hours</span>
+                </span>
+            </div>    
+        </div>
+
+        <div class="course-name-description">
+            <h3>${course.title}</h3>
+            <p>${course.description}</p>
+        </div>
+        <div class="course-teacher-price">
+            <div class="course-teacher">
+                <a href="#">
+                    <img src="${course.teacher_image}" alt="teacher image">
+                    <p>${course.teacher}</p>
+                </a>
+            </div>
+            <a href="#">
+                <div class="course-price price-${priceClass}">
+                    <span>${price}</span>
+                </div>
+            </a>
+        </div>
+    </div>
+        `);
     });
 
 }
