31d99e0b82aaa8119b75d179a036dc89d45bb5c2
Milenamaka <milena85.mm@yahoo.com>
2021-04-05 17:34:01 +0200

    teachers

--- js/projectSlider.js
+++ js/projectSlider.js
@@ -22,19 +22,31 @@ function renderSlider(arr) {
   $('.slider').append(
   
       
-`<div class="container">
- <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
+`<div class="container w-100 ">
+ <div id="myCarousel" class="carousel slide w-100" data-ride="carousel">
+  <div class="carousel-inner w-100">
     <div class="item active w-100">
-      <img src="${arr[0].image}" alt="${arr[0].title}" style="width:100%;">
+      <img src="${arr[0].image}" alt="" style="width:100%;">
+      <div class="carousel-caption">
+      <h3>${arr[0].title}</h3>
+      <p>${arr[0].description}</p>
+    </div>
     </div>
 
-    <div class="item">
-      <img src="${arr[1].image}" alt="${arr[1].title}" style="width:100%;">
+    <div class="item w-100">
+      <img src="${arr[1].image}" alt="" style="width:100%;">
+      <div class="carousel-caption">
+      <h3>${arr[1].title}</h3>
+      <p>${arr[1].description}</p>
+    </div>
     </div>
   
-    <div class="item">
-      <img src="${arr[2].image}" alt="${arr[2].title}" style="width:100%;">
+    <div class="item w-100">
+      <img src="${arr[2].image}" alt="" style="width:100%;">
+      <div class="carousel-caption">
+      <h3>${arr[2].title}</h3>
+      <p>${arr[2].description}</p>
+      </div>
     </div>
   </div>
 
@@ -48,8 +60,5 @@ function renderSlider(arr) {
     <span class="sr-only">Next</span>
   </a>
 </div>
-</div>
-      
-      
-`)}
+</div>`)}
         
